614f171ebb37cfda1b68fd8ef7b164c94d280ab4
yaron <yarongerstel@windowslive.com>
2022-06-22 01:53:48 +0300

    ngrok

--- TelegramContact.py
+++ TelegramContact.py
@@ -11,7 +11,8 @@ class TelContact:
     """Department responsible for communication with the Telegram client"""
     # argv[1] contain the api_key of the bot
     # NGROK is used to make the server public
-    TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}'.format(sys.argv[1])
+    TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}' \
+                                '/setWebhook?url=https://7834-79-179-181-57.eu.ngrok.io'.format(sys.argv[1])
     app = Flask(__name__)
 
     @staticmethod
