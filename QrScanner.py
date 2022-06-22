d06512ce27f909bb91c5b9420cece67536e8432c
yaron <yarongerstel@windowslive.com>
2022-06-22 22:25:54 +0300

    Some notes

--- QrScanner.py
+++ QrScanner.py
@@ -2,6 +2,7 @@ from pyzbar import pyzbar
 from PIL import Image
 
 
+
 class QrDecoder:
     """A class that receives an image that contains a QR barcode and returns a decoding of the barcode"""
     def __init__(self, image):
@@ -11,9 +12,8 @@ class QrDecoder:
         """A function that receives an image that contains a QR barcode and returns decoding of the barcode"""
         try:
             img = Image.open(self._image)
-            qr_code = pyzbar.decode(img)[0]
-            # convert into string
-            data = qr_code.data.decode("utf-8")
+            qr_code = pyzbar.decode(img)[0]  # Receives an object that contains information about the decryption
+            data = qr_code.data.decode("utf-8")  # Extracts information about decode data from decode object
             return data
         except :
             return "fail reading image"
--- TelegramContact.py
+++ TelegramContact.py
@@ -12,7 +12,7 @@ class TelContact:
     # argv[1] contain the api_key of the bot
     # NGROK is used to make the server public
     TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}' \
-                                '/setWebhook?url=https://7834-79-179-181-57.eu.ngrok.io'.format(sys.argv[1])
+                                '/setWebhook?url=https://88bd-79-179-181-57.eu.ngrok.io'.format(sys.argv[1])
     app = Flask(__name__)
 
     @staticmethod
